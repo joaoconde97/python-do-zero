bb76dfeac1eba08f7d368290c656b6f6227fd0a1
joaoconde <jcondedev@gmail.com>
2026-01-20 20:26:43 -0300

    Implementando as funções

--- analisador-de-numeros.py
+++ analisador-de-numeros.py
@@ -1,50 +1,52 @@
 lancamentos = []
 
-while True:
+def mostrar_menu():
 
+    print("***********************")
     print("Escolha um opção")
     print("1 - Inserir novo valor")
     print("2 - Analisar")
-    print("3 - Sair")
+    print("3 - Limpar")
+    print("4 - Sair")
+    print("***********************")
 
-    opcao = input("Digite a opção desejada: ")
+def inserir_valor():
 
-    if opcao == "1":
-        valor = int(input("Insira um valor: "))
+    valor = int(input("Insira um valor: "))
 
-        lancamentos.append({
-            "valor": valor,
+    lancamentos.append({
+        "valor": valor,
         
-        })
-        
-    elif opcao == "2" :
-        quant_impar = 0
-        quant_par = 0
-        soma = 0
+    })
+
+def analisar_lancamentos():
+
+    quant_impar = 0
+    quant_par = 0
+    soma = 0
+    maior = lancamentos[0]["valor"]
+
+    if len(lancamentos) == 0:
+        print("Nenhum valor inserido.")
+
+    else :
         maior = lancamentos[0]["valor"]
+        for item in lancamentos :
+            valor = item["valor"]
 
-        if len(lancamentos) == 0:
-            print("Nenhum valor inserido.")
+            if valor % 2 == 0 :
+                quant_par += 1
 
-        else :
-            maior = lancamentos[0]["valor"]
-            for item in lancamentos :
-                valor = item["valor"]
+            else :
+                quant_impar += 1
 
-                if valor % 2 == 0 :
-                    quant_par += 1
+            soma += valor
 
-                else :
-                    quant_impar += 1
+            if valor > maior:
+                maior = valor
+        
+        media = soma / len(lancamentos)
 
-                soma += valor
-
-                if valor > maior:
-                    maior = valor
-            
-            media = soma / len(lancamentos)
-
-    
         print(f"A soma dos valores inseridos é de {soma}.")
 
         print(f"Dos quais são {quant_par} Par e {quant_impar} Ímpar.")
@@ -53,7 +55,28 @@ while True:
 
         print(f"A média dos valores inseridos é {media}.")
 
+def limpar_lancamentos():
+    confirmacao = input("Você realmente quer apagar todos os lançamentos? s/n ")
+
+    if confirmacao == "s":
+        lancamentos.clear()
+        print("A lista foi resetada.")
+    
+
+while True:
+    mostrar_menu()
+    opcao = input("Digite a opção desejada: ")
+    print(" ")
+
+    if opcao == "1":
+        inserir_valor()
+        
+    elif opcao == "2" :
+        analisar_lancamentos()
 
     elif opcao == "3" :
+        limpar_lancamentos()
+        
+    elif opcao == "4" :
         break 
 
